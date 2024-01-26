d43ac2b1d8d33bc8250d9d3f2c63a21ef6c83bb1
Technofancy Learner <vkhatri137@gmail.com>
2024-01-26 10:38:23 +0545

    modified:   with-JS/index.js

--- with-JS/index.js
+++ with-JS/index.js
@@ -28,3 +28,38 @@ for (let count=0; count<=(todos.length-1); count++) {
 };
 
 console.log("Ended");
+
+let names = ["Ram", "Sita", "Shyam", "Radha" , "Hari", "Laxmi"];
+for (let count=0; count < names.length; count++) {
+    console.log(`Bho ${names[count]}`);
+}
+
+
+function multiply(Input, start, end) {
+    for (let count = start; count <= end; count++) {
+        console.log(`${Input} * ${count} = ${Input*count}`);
+    }
+}
+multiply(5, 1, 10);
+multiply(10, 5, 10);
+
+let usersData = [
+    {name:"Ram", password: 1234},
+    {name:"Sita", password: 12345},
+    {name:"Laxman", password: 123456},
+]
+
+function logIn(userName, passKey) {
+    for (let index = 0; index < usersData.length; index++)
+    {
+        if (userName == usersData[index].name && passKey == usersData[index].password) {
+            return `Hey ${usersData[index].name}, you're welcome.`
+        }
+    }
+    return 'Invalid UserName or Password'
+}
+
+console.log(logIn("Ram", 1234));
+console.log(logIn("Sita", 12345));
+console.log(logIn("Laxman", 123456));
+console.log(logIn("Ramm", 12345));
